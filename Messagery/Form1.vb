86f664c331a514bd5596f0d864e4fe2ac5e0c888
vince <vince@DESKTOP-KQ3MCQF>
2026-03-29 10:37:14 -0400

    TMP

--- Messagery/Form1.vb
+++ Messagery/Form1.vb
@@ -1,3 +1,13 @@
 ﻿Public Class Form1
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
 
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
 End Class
