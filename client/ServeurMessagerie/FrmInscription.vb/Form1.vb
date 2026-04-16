bc656e6b63e56c81ce40c3dd28f677c255d27c51
a <a@a>
2026-04-16 03:47:03 -0400

    Retiré quelques fichiers superflus

--- client/ServeurMessagerie/FrmInscription.vb/Form1.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class FrmInscription
-    Private Sub FrmInscription_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-End Class
--- client/ServeurMessagerie/FrmMessagerie.vb/Form1.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class FrmMessagerie
-    Private Sub FrmMessagerie_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-End Class
