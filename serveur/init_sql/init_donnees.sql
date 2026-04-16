f1f7f98db243b47a21bc5b0c2fd4182928441b07
a <a@a>
2026-04-16 06:37:48 -0400

    Terminé le rapport

--- serveur/init_sql/init_donnees.sql
+++ serveur/init_sql/init_donnees.sql
@@ -1,12 +1,12 @@
 INSERT INTO Utilisateurs VALUES
-(1,"test1","test1","mdp_test1",NULL,NULL),
-(2,"test2","test2","mdp_test2",NULL,NULL),
-(3,"test3","test3","mdp_test3",NULL,NULL);
+(1,"jed02","Jean Dufour","mdp_jed02",NULL,NULL),
+(2,"mar06","Marc-André Roubiot","mdp_mar06",NULL,NULL),
+(3,"pam03","Paul Martin","mdp_pam03",NULL,NULL);
 
 INSERT INTO Contacts VALUES
-(1,"test1","test1",NULL,1,1),
-(2,"test2","test2",NULL,1,2),
-(3,"test3","test3",NULL,1,3);
+(1,"jed02","Jean Dufour",NULL,1,1),
+(2,"mar06","Marc-André Roubiot",NULL,1,2),
+(3,"pam03","Paul Martin",NULL,1,3);
 
 INSERT INTO Conversations VALUES (1), (2);
 
@@ -14,7 +14,7 @@ INSERT INTO ConversationsContacts (contact_id, conversation_id) VALUES
 (1,1), (2,1), (2,2), (3,2);
 
 INSERT INTO Messages (message_id, conversation_id, contact_id, date, message) VALUES
-(1,1,1,"2026-04-13T15:24:00","Bonjour"),
-(2,1,2,"2026-04-13T15:24:05","Bonjour"),
-(3,2,2,"2026-04-13T15:24:10","Bonjour"),
-(4,2,3,"2026-04-13T15:24:15","Bonjour");
+(1,1,1,"2026-04-13T15:24:00","Bonjour Marc"),
+(2,1,2,"2026-04-13T15:24:05","Bonjour Jean"),
+(3,2,2,"2026-04-13T15:24:10","Bonjour Paul"),
+(4,2,3,"2026-04-13T15:24:15","Bonjour Marc");
